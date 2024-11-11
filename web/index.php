61137f8a6cce4c5cea0c5dc4df0f862f13410590
Илья Ли <ilia.lee.573@gmail.com>
2024-11-11 22:32:20 +0200

    fit: add pdo connection

--- web/index.php
+++ web/index.php
@@ -1,7 +1,19 @@
 <?php
 
-$var = 'Hello World!';
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-$var = 'he';
+//if($requestMethod == 'GET') {
+//
+//}
 
-echo $var;
+$tableName = 'Currencies';
+
+$pdo = new PDO('mysql:host=mysql;dbname=app', 'user', 'secret');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$stmt = $pdo->query("SELECT * FROM $tableName WHERE ID = 1 ");
+while ($row = $stmt->fetch())
+{
+   echo "<pre>";
+    print_r($row);
+   echo "</pre>";
+}
